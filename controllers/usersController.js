ee675d04bd816cff6793e75e571f7766c29db2cc
Manderson3568 <manderson3568@outlook.com.au>
2023-07-10 21:25:57 +1000

    Login info not updating on front end bug fixed

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,18 +5,23 @@ export async function loginUser(req, res) {
     try {
         const result = await pool.query('SELECT * FROM users WHERE email= $1',[req.body.email]);
         let user = result.rows[0]
+        console.log("user" ,req.body)
         if(user.password === req.body.password){
             console.log(user)
     
             const payload= {
               "id":user.id,
               'name': user.name,
-              'admin': user.admin
+              'admin': user.admin,
+              'login':true
             }
 
             jwt.sign(payload, "shhh", {expiresIn:"10h"}, (err,token)=>{
               res.json(
-                {token:token}
+                {
+                  token:token,
+                  payload: payload
+                }
               )
             })
           }
